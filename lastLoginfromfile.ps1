e49b6e15fa36b1cb9710e97c090b59efb199cec2
NIHD <102390433+NIHDITS@users.noreply.github.com>
2022-03-25 14:53:34 -0700

    Create lastLoginfromfile.ps1

---
+++ lastLoginfromfile.ps1
@@ -0,0 +1,6 @@
+ $file = Get-Content -Path "C:\BGinfo\users.txt"
+ foreach ($user in $file)
+     {
+     Get-ADUser -Identity $user -Properties LastLogonDate | Select-Object SamAccountName, LastLogonDate
+     }
+
